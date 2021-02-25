d06a817c94bf3c2fc2bee35db4b6fa05c5c8adcb
Grant <gmacdabeast@Grants-MacBook-Pro.local>
2021-02-25 16:52:11 -0600

    new number guessing

---
+++ NumberGuessing.py
@@ -0,0 +1,31 @@
+from random import *
+
+n = randint(0,10)
+tries = 10
+print('you get 10 tries to figure it out.')
+numberGuess = int(input('Guess what the number is here: '))
+
+while n != 'numberGuess':
+    if (tries > 1):
+        if numberGuess <= 100:
+            if (numberGuess < n):
+                tries += -1
+                print('Number is too low! Try again! You now have ' + str(tries) + ' left')
+                numberGuess = int(input('Guess what the number is here: '))
+            elif (numberGuess > n):
+                tries += -1
+                print('Number is too high! Try again! You now have ' + str(tries) + ' left')
+                numberGuess = int(input('Guess what the number is here: '))
+            else:
+                break
+        else:
+            print('cannnot be over 100! Try again.')
+            numberGuess = int(input('Guess what the number is here: '))
+    else:
+        print('you suck ass and could not guess! START OVER BITCH')
+        exit()
+
+
+
+
+print('you did it! You guessed the number ' + str(n))
